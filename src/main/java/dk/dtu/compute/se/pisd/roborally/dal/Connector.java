1ac14271c1d5300b5e0916cf79d7c060f7e7ca95
abheyde <s193576@student.dtu.dk>
2021-05-11 16:08:46 +0200

    bumbum

--- src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Connector.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Connector.java
@@ -41,7 +41,7 @@ class Connector {
     private static final int    PORT     = 3306;
     private static final String DATABASE = "pisu";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Fodbold12";
+    private static final String PASSWORD = "1234sander";
 
     private static final String DELIMITER = ";;";
     
